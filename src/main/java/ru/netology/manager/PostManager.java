b118a2f222081dfeb2f13bcab7243e329df37c15
Uliana Utina <ulyana190909@yandex.ru>
2020-09-06 22:34:54 +0300

    корректировка второго задания

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -4,22 +4,11 @@ import ru.netology.domain.Post;
 
 public class PostManager{
     private Post post;
-    private int ownerId = -1;     //идентификатор пользователя или сообщества, на стене которого находится запись.
-    private int postId;      //идентификатор записи на стене
 
-    private int idOwner;     //идентификатор пользователя или сообщества
-    private int domain;      //короткий адрес пользователя или сообщества.
-    private String query;    //поисковой запрос. Для точного результата запрос необходимо передавать в двойных кавычках
-    private int ownersOnly;  //возвращать только записи от имени владельца стены
-    private int count;       //количество записей, которые необходимо вернуть.
-    private int offset;      //смещение, необходимо для получения определенного подмножества результатов
-
-    // + get/set на все поля
 
     public Post[] search() {
-        Post post = new Post();
-    //TODO:: add logic
-        return Post [];
+        //TODO:: add logic
+        return null;
     }
 
     public void delete () {
